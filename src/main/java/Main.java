c6ed23fa4e220efa09b4efe90e1442b41e29af8f
PaulKreft <64103063+PaulKreft@users.noreply.github.com>
2023-12-05 15:56:25 +0100

    implement add and remove book functionality

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,16 +3,41 @@ import de.neuefische.Library;
 
 public class Main {
     public static void main(String[] args) {
-        Book[] favouriteBooks = new Book[5];
+        Book[] favouriteBooks = new Book[4];
 
-        favouriteBooks[0] = new Book("Paul Kreft", "My Java Journey - Revised (buy this one, the other one is shit)", "1");
-        favouriteBooks[1] = new Book("Tamana Sherzad", "My Java Journey", "2");
-        favouriteBooks[2] = new Book("Vincent Kahl", "Die Leiden des jungen Programmierers", "3");
-        favouriteBooks[3] = new Book("Aslon Badridinov", "Meine Erfahrungen mit der Programmiersprache Java", "4");
-        favouriteBooks[4] = new Book("Elvedin Turkovic", "How to teach Java perfectly", "5");
+        favouriteBooks[0] = new Book("Tamana Sherzad", "My Java Journey", "2");
+        favouriteBooks[1] = new Book("Vincent Kahl", "Die Leiden des jungen Programmierers", "3");
+        favouriteBooks[2] = new Book("Aslon Badridinov", "Meine Erfahrungen mit der Programmiersprache Java", "4");
+        favouriteBooks[3] = new Book("Elvedin Turkovic", "How to teach Java perfectly", "5");
 
         Library alexandria = new Library("Alexandria", favouriteBooks);
 
+
+        Book plagiatedBook = new Book("Paul Kreft", "My Java Journey - Revised (buy this one, the other one is shit)", "1");
+
+        // Initial state of library
+        System.out.println("Initial state of library");
+        System.out.println(alexandria);
+        System.out.println();
+
+        // Should not work, since library is full
+        alexandria.addBook(plagiatedBook);
+        System.out.println();
+
+        // deleting one book
+        alexandria.deleteBookByTitle("How to teach Java perfectly");
+        System.out.println();
+
+        // trying to delete the same book again - should print error
+        alexandria.deleteBookByTitle("How to teach Java perfectly");
+        System.out.println();
+
+        // Adding of new book should work now, since library has a free slot
+        alexandria.addBook(plagiatedBook);
+        System.out.println();
+
+        // print final state of library
+        System.out.println("Final state of library");
         System.out.println(alexandria);
     }
 }
--- src/main/java/de/neuefische/Library.java
+++ src/main/java/de/neuefische/Library.java
@@ -11,6 +11,30 @@ public class Library {
         this.books = books;
     }
 
+    public void addBook(Book book) {
+        for (int i = 0; i < this.books.length; i++) {
+            if(books[i] == null) {
+                books[i] = book;
+                System.out.println("Book added: " + book.toString());
+                return;
+            }
+        }
+
+        System.out.println("Sorry, library is full...");
+    }
+
+    public void deleteBookByTitle(String bookTitle) {
+        for (int i = 0; i < this.books.length; i++) {
+            if(this.books[i] != null && this.books[i].getTitle().equals(bookTitle)) {
+                this.books[i] = null;
+                System.out.println("Book remove: " + bookTitle);
+                return;
+            }
+        }
+
+        System.out.println("Book not found");
+    }
+
     @Override
     public String toString() {
         return "Library{" + "name='" + name + '\'' + ", books=" + Arrays.toString(books) + '}';
